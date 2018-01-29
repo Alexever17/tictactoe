c4e8f30def332cc46a94d43b7dcc31b48fc04b16
alexever17@gmail.com <alexever17@gmail.com>
2018-01-29 22:48:10 +0000

    intermediary update, the ai got a bit more clever

--- master.js
+++ master.js
@@ -1,7 +1,11 @@
+var clickCounter = 0;
+var storage;
+
 
 function clickButton(tile) {
     var test = tileTest(tile);
     if (test) {
+        clickCounter += 1;
         write(tile, "X");
         var check9 = checkNine();
         if (check9) {
@@ -9,18 +13,156 @@ function clickButton(tile) {
             return;
         }
         var checkX = checkSystem(tile, "X");
+        console.log(checkX);
         if (checkX.includes(3)) {
             gameFinish();
             return;
         }
-        var tileO = oResponseRandom();
+        var tileO = oResponse(tile, checkX);
         var checkO = checkSystem(tileO, "O");
-        console.log(checkO);
         if (checkO.includes(3)) {
             gameFinish();
             return;
         }
     }
+}
+
+function oResponse(tile, checkX) {
+    var tileO;
+    switch (clickCounter) {
+        case 1:
+            tileO = oResponseRandom();
+            break;
+        case 2:
+        case 3:
+        case 4:
+            tileO = twoXlocator(tile, checkX);
+            break;
+    }
+    return tileO;
+}
+
+function twoXlocator(tile, checkX) {
+    var tileO;
+    var test;
+    var counter = 0;
+    rowNumber = (tile.parentElement.id.slice(3) - 1);
+    if (checkX[0] == 2) {
+        for (var i = 1; i < 4; i++) {
+            number1 = i + (rowNumber*3);
+            test = tileTest(document.getElementsByClassName("i" + number1)[0]);
+            if (test) {
+                tileO = document.getElementsByClassName("i" + number1)[0];
+                write(tileO, "O");
+                return tileO;
+            }
+        }
+        counter += 1;
+    } else {
+        counter += 1;
+    }
+    if (checkX[1] == 2) {
+        columnNumber = Number(tile.classList[2].slice(1));
+        switch (columnNumber) {
+            case 1:
+            case 4:
+            case 7:
+                columnNumber = 0;
+                break;
+            case 2:
+            case 5:
+            case 8:
+                columnNumber = 1;
+                break;
+            case 3:
+            case 6:
+            case 9:
+                columnNumber = 2;
+                break;
+        }
+        for (var j = 1; j < 10; j += 3) {
+            number2 = j + columnNumber;
+            test = tileTest(document.getElementsByClassName("i" + number2)[0]);
+            if (test) {
+                tileO = document.getElementsByClassName("i" + number2)[0];
+                write(tileO, "O");
+                return tileO;
+            }
+        }
+        counter += 1;
+    } else {
+        counter += 1;
+    }
+    if (checkX[2] == 2) {
+        diagonalNumber = Number(tile.classList[2].slice(1));
+        switch (diagonalNumber) {
+            case 2:
+            case 4:
+            case 6:
+            case 8:
+                console.log("2468");
+                break;
+            case 1:
+            case 9:
+                console.log("19");
+                for (var k = 1; k < 10; k += 4) {
+                    number3 = k;
+                    test = tileTest(document.getElementsByClassName("i" + number3)[0]);
+                    if (test) {
+                        tileO = document.getElementsByClassName("i" + number3)[0];
+                        write(tileO, "O");
+                        return tileO;
+                    }
+                }
+                break;
+            case 3:
+            case 7:
+                console.log("37");
+                for (var r = 3; r < 8; r += 2) {
+                    number4 = r;
+                    test = tileTest(document.getElementsByClassName("i" + number4)[0]);
+                    if (test) {
+                        tileO = document.getElementsByClassName("i" + number4)[0];
+                        write(tileO, "O");
+                        return tileO;
+                    }
+                }
+                break;
+            case 5:
+                console.log("5");
+                if (document.getElementById('one').innerHTML == "X" || document.getElementById('nine').innerHTML == "X") {
+                    for (var v = 1; v < 10; v += 4) {
+                        number5 = v;
+                        test = tileTest(document.getElementsByClassName("i" + number5)[0]);
+                        if (test) {
+                            tileO = document.getElementsByClassName("i" + number5)[0];
+                            write(tileO, "O");
+                            return tileO;
+                        }
+                    }
+                }
+                if (document.getElementById('three').innerHTML == "X" || document.getElementById('seven').innerHTML == "X") {
+                    for (var x = 3; x < 8; x += 2) {
+                        number6 = r;
+                        test = tileTest(document.getElementsByClassName("i" + number6)[0]);
+                        if (test) {
+                            tileO = document.getElementsByClassName("i" + number6)[0];
+                            write(tileO, "O");
+                            return tileO;
+                        }
+                    }
+                }
+                break;
+        }
+        counter += 1;
+    } else {
+        counter += 1;
+    }
+    if (counter > 2) {
+        console.log("s");
+        tileO = oResponseRandom();
+    }
+    return tileO;
 }
 
 function oResponseRandom() {
@@ -173,6 +315,20 @@ function diagonalCheck(tile, letter) {
             value += 1;
         }
     }
+    if (number == 5) {
+        if (document.getElementsByClassName('i1')[0].innerHTML == letter) {
+            value += 1;
+        }
+        if (document.getElementsByClassName('i9')[0].innerHTML == letter) {
+            value += 1;
+        }
+        if (document.getElementsByClassName('i3')[0].innerHTML == letter) {
+            value += 1;
+        }
+        if (document.getElementsByClassName('i7')[0].innerHTML == letter) {
+            value += 1;
+        }
+    }
     if (document.getElementsByClassName('i5')[0].innerHTML == letter) {
         value += 1;
     }
@@ -186,6 +342,7 @@ function gameFinish() {
 }
 
 function newGame() {
+    clickCounter = 0;
     for (var i = 0; i < 9; i++) {
         document.getElementsByClassName("field")[i].innerHTML = "";
         document.getElementsByClassName("field")[i].disabled = false;
